c7671de6fc8d3cb1532ce5a8566b7a781be3fee0
Cristian Dias <cristian.dias@sap.com>
2020-10-28 21:16:52 -0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['http://localhost:3000', 'https://meutreinador.netlify.app'] });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({

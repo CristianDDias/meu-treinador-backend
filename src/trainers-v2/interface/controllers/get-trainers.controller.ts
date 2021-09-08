dea046f033f1178dd1c27161a1cc95fd4ecba0ef
Cristian Dias <c-2d@hotmail.com>
2021-09-08 07:29:26 -0300

    WIP

--- src/trainers-v2/interface/controllers/get-trainers.controller.ts
+++ src/trainers-v2/interface/controllers/get-trainers.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Query, DefaultValuePipe } from '@nestjs/common';
 import { GetTrainersUseCase } from '../../application/use-cases/get-trainers.use-case';
 import { TrainerDTOAdapter, SimplifiedTrainerDTO, Pagination } from '../dtos/trainer.dto';
-import { TrainerQueryFilter } from '../../domain/trainer.repository';
 import { ParsePaginationPipe } from '../pipes/parse-pagination.pipe';
 
 @Controller('test/trainers')
@@ -15,7 +14,9 @@ export class GetTrainersController {
     @Query('name') name?: string,
   ) {
     const { results, total } = await this.useCase.execute({
-      filter: this.buildFilter(name),
+      filter: {
+        name: name ? new RegExp(name, 'i') : undefined,
+      },
       pagination: { page, limit },
     });
 
@@ -25,13 +26,5 @@ export class GetTrainersController {
       page,
       totalPages: Math.ceil(total / limit),
     } as Pagination<SimplifiedTrainerDTO>;
-  }
-
-  private buildFilter(name?: string) {
-    const filter: TrainerQueryFilter = {};
-    if (name) {
-      filter.name = new RegExp(name, 'i');
-    }
-    return filter;
   }
 }
